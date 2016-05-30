e66aec8d0a5eaa82f1b56eb7672385b63590bc86
György Gáspár <igeorge1982@gmail.com>
2016-05-30 22:13:03 +0200

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by Gaspar Gyorgy on 30/05/16.
+//  Copyright © 2016 Gaspar Gyorgy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
